f802f9d31c2d94a59c93422f387a72a17a756508
Austin <austin.fratesi@gmail.com>
2025-06-10 21:28:31 -0500

    update 6-10-25

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <form className="mainForm">
+      <input type='text' placeholder="i.e bacon"></input>
+      <button>+ Add Ingredients</button>
+    </form>
+
+  )
+}
