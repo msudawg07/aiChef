7e49c8b3a6caf3e513718d5da6436c003f99d9ac
Austin <austin.fratesi@gmail.com>
2025-06-12 22:10:47 -0500

    latest commit 6-12-25

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
+
 export default function Main() {
+
+  const [listOfIngredients, setListOfIngredients] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget)
+    const ingredient = formData.get('ingredient')
+    setListOfIngredients((prev) => [...prev, <li>{ingredient}</li>])
+  }
+
   return (
     <main>
-      <form className="mainForm">
-        <input type='text' placeholder="i.e bacon"></input>
+      <form onSubmit={handleSubmit} className="mainForm">
+        <input type='text' placeholder="i.e bacon" name='ingredient'></input>
         <button>+ Add Ingredients</button>
       </form>
-    </main>
 
+      <ul>{listOfIngredients}</ul>
+    </main>
 
   )
 }
