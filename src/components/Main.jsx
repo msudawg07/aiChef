7371e3c307b55eb55d24e0dc76779e9177d14e9c
Austin <austin.fratesi@gmail.com>
2025-06-19 07:47:38 -0500

    latest updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export default function Main() {
 
   const [listOfIngredients, setListOfIngredients] = useState([])
-  const [onHand, setonHand] = useState('')
+  // const [onHand, setonHand] = useState('') line 35
 
   // function handleSubmit(e) {
   //   e.preventDefault();
@@ -15,19 +15,31 @@ export default function Main() {
 
   function submit(formData) {
     let ingredient = formData.get('ingredient')
+    if(ingredient.startsWith(" ")) {
+      alert('Must enter ingredient')
+      return
+    }
+    console.log(ingredient)
     setListOfIngredients(prev => [...prev, <li>{ingredient}</li>])
-    setonHand('Ingredients on hand')
+    // setonHand('Ingredients on hand') line 35
+  }
+
+  function h2() {
+    return (
+      listOfIngredients.length != 0 && <h2>List of ingredients</h2>
+    )
   }
 
   return (
     <main>
       <form action={submit} className="mainForm">
-        <input type='text' placeholder="i.e bacon" name='ingredient'></input>
+        <input type='text' placeholder="i.e bacon" name='ingredient' required></input>
         <button>+ Add Ingredients</button>
       </form>
 
       {/* <ul>{listOfIngredients.map(item => <li>{item}</li>)}</ul> */}
-      <h1>{onHand}</h1>
+      {/* <h1>{listOfIngredients}</h1> */}
+      {h2()}
       <ul>{listOfIngredients}</ul>
     </main>
 
